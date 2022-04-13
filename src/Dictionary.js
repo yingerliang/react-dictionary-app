8b61b53990162133508571e87f0da156cf8a419c
yinger <75009049+yingerliang@users.noreply.github.com>
2022-04-13 19:56:33 +1200

    Rendered dictionary meanings

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,21 @@
 import React, { useState } from 'react'
 import axios from 'axios'
 import './Dictionary.css'
+import Results from './Results'
 
 export default function Dictionary() {
   const [keyword, setKeyword] = useState('')
+  const [results, setResults] = useState(null)
+
+  function handleResponse(response) {
+    setResults(response.data[0])
+  }
 
   function search(event) {
     event.preventDefault()
+
+    const apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`
+    axios.get(apiUrl).then(handleResponse)
   }
 
   function handleChange(event) {
@@ -18,6 +27,7 @@ export default function Dictionary() {
       <form onSubmit={search}>
         <input type="Dictionary" onChange={handleChange} autoFocus={true} />
       </form>
+      <Results results={results} />
     </div>
   )
 }
