1c3bf7bf841f7ee108f8587895ccb9751e2781cd
yinger <75009049+yingerliang@users.noreply.github.com>
2022-04-13 19:06:18 +1200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css'
+import logo from './images/book.gif'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo img-fluid" alt="logo" />
+        <h1> Dictionary </h1>
+      </header>
+    </div>
+  )
+}
+
+export default App
